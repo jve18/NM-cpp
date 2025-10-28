14c891272a48dbb2292465932ff676601a4f1ad2
jve18 <jay.v.evans1736@gmail.com>
2025-10-27 22:39:27 -0400

    Re-organize bisection/secant testing

---
+++ test/root/test_fzero_bisect/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include "../../../source/NM-cpp.h"
+
+double y(double x)
+{
+  return pow((x+2.0),2.0) - 3;
+}
+
+int main()
+{
+
+  double x_L = -2.0; // Lower bound guess
+  double x_U = 0.0; // Upper boung guess
+  double eps_y = pow(10.0,-4.0); // Error tolerance
+  int i_max = 10000; // Max number of iterations
+  double x_root;
+
+  x_root = fzero_bisect(y,x_L,x_U,eps_y,i_max);
+
+  std::cout << x_root << std::endl;
+
+  return 0;
+  
+  
+
+}
